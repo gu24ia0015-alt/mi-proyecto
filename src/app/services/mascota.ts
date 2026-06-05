3bf9219749696680dcae206837c20a4f9efae034
gu24ia0015-alt <gu24ia0015@globaluniversity.edu.mx>
2026-06-05 11:12:33 -0600

    feat: (mascota.service.ts) - add GET POST DELETE methods

--- src/app/services/mascota.ts
+++ src/app/services/mascota.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Mascota } from '../interfaces/mascota';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Mascota {}
+export class MascotaService {
+
+  private url = 'http://127.0.0.1:5000';
+
+  constructor(private http: HttpClient) {}
+
+  getMascotas() {
+    return this.http.get<Mascota[]>(`${this.url}/mascotas`);
+  }
+
+  agregarMascota(mascota: Mascota) {
+    return this.http.post<Mascota>(`${this.url}/mascotas`, mascota);
+  }
+
+  eliminarMascota(id: number) {
+    return this.http.delete(`${this.url}/mascotas/${id}`);
+  }
+
+}
