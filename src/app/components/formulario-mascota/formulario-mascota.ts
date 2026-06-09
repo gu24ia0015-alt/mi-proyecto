2cd3315eb1a5cdc5dada797d7a8fc257a0003966
gu24ia0015-alt <gu24ia0015@globaluniversity.edu.mx>
2026-06-08 20:49:56 -0600

    feat: (formulario-mascota.ts) - add form logic with Output emitter

--- src/app/components/formulario-mascota/formulario-mascota.ts
+++ src/app/components/formulario-mascota/formulario-mascota.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Mascota } from '../../interfaces/mascota';
+import { MascotaService } from '../../services/mascota';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-mascota',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './formulario-mascota.html',
-  styleUrl: './formulario-mascota.css',
+  styleUrl: './formulario-mascota.css'
 })
-export class FormularioMascota {}
+export class FormularioMascotaComponent {
+
+  @Output() mascotaGuardada = new EventEmitter<void>();
+
+  nuevaMascota: Mascota = {
+    id: 0,
+    nombre: '',
+    raza: '',
+    edad: 0
+  };
+
+  constructor(private mascotaService: MascotaService) {}
+
+  agregarMascota() {
+    this.mascotaService.agregarMascota(this.nuevaMascota).subscribe(() => {
+      this.mascotaGuardada.emit();
+      this.nuevaMascota = { id: 0, nombre: '', raza: '', edad: 0 };
+    });
+  }
+
+}
